ea376f5322219273e870ba85ae7f6328d2736e78
Harada <helioharada30@gmail.com>
2020-02-19 10:04:30 -0300

    server 3

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const
-  express = require('express'),
-  serveStatic = require('serve-static'),
-  history = require('connect-history-api-fallback'),
-  port = process.env.PORT || 5000
 
-const app = express()
+// importa configuração do Express e do banco de dados
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
 
-app.use(history())
-app.use(serveStatic(__dirname + '/dist/spa'))
-app.listen(port)
+app = express ();
+app.use(serveStatic(__dirname));
+
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, '/dist/spa');
+    console.log(index);
+    res.sendFile(index)
+});
+
+var port = process.env.PORT || 21058;
+// passa a porta definida no Express e levantamento do servidor
+app.listen(port);
+console.log ('Servidor client iniciado na porta ' + port + '.');
