3c516e77263d8ac6bf72885cf3db5501bd64ab1d
Harada <helioharada30@gmail.com>
2020-03-05 10:57:49 -0300

    update file name

--- server.js
+++ server.js
@@ -12,8 +12,13 @@ app.use(history())
 app.use(serveStatic(__dirname + '/dist/spa'))
 
 
+app.all('/*', function(req, res) {
+
+  res.sendFile(path.resolve('/dist/spa/index.html'));
+});
 
 var port = process.env.PORT || 8181;
 // passa a porta definida no Express e levantamento do servidor
 app.listen(port);
 console.log ('Servidor client iniciado na porta ' + port + '.');
+
